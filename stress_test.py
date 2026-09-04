4d56484ebf72913ed5f2267df8cab0237666b6b9
woraviriya008 <woraviriyanoulnok@gmail.com>
2026-09-04 05:20:38 +0000

    Lab1 OS

--- stress_test.py
+++ stress_test.py
@@ -2,7 +2,11 @@
 # OS-Lab 01: CPU Stress Test
 # Student ID: [Enter your ID here]
 # ==========================================
-import time
+import math
+
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+    math.factorial(50000) # Intensive calculation
 
 # TODO: Write the intensive computation loop here
 # Follow the instructions in the Lab manual.
