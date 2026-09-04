393362b68945f3544ee37e9a24ff139f5f78e304
woraviriya008 <woraviriyanoulnok@gmail.com>
2026-09-04 05:01:27 +0000

    update code

--- profiler.py
+++ profiler.py
@@ -1,10 +1,7 @@
-# ==========================================
-# OS-Lab 01: System Profiler
-# Student ID: [Enter your ID here]
-# ==========================================
 import os
-import platform
 import psutil
+import platform
 
-# TODO: Write your system profiler code here 
-# Follow the instructions in the Lab manual.
+print(f"OS Name: {platform.system()} {platform.release()}")
+print(f"Number of CPU Cores: {psutil.cpu_count(logical=True)}")
+print(f"Total RAM: {round(psutil.virtual_memory().total / (1024**3), 2)} GB")
